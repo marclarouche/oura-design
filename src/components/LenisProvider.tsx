9aa82365414d14a1c9a84e451f0aefddd44f7944
Marc <17996316+marclarouche@users.noreply.github.com>
2026-06-29 14:56:07 -0700

    Update project files

--- src/components/LenisProvider.tsx
+++ src/components/LenisProvider.tsx
@@ -1,31 +1,17 @@
 "use client";
-
-import { useEffect } from "react";
-import Lenis from "lenis";
+import { ReactLenis } from 'lenis/react';
 
 export function LenisProvider({ children }: { children: React.ReactNode }) {
-    useEffect(() => {
-        const lenis = new Lenis({
-            duration: 1.2,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // smooth scrolling
-            orientation: "vertical",
-            gestureOrientation: "vertical",
-            smoothWheel: true,
-            wheelMultiplier: 1,
-            touchMultiplier: 2,
-        });
-
-        function raf(time: number) {
-            lenis.raf(time);
-            requestAnimationFrame(raf);
-        }
-
-        requestAnimationFrame(raf);
-
-        return () => {
-            lenis.destroy();
-        };
-    }, []);
-
-    return <>{children}</>;
+  return (
+    <ReactLenis 
+      root 
+      options={{ 
+        duration: 1.2, 
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        smoothWheel: true 
+      }}
+    >
+      {children}
+    </ReactLenis>
+  );
 }
